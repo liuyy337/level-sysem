1ae3b6cabb7ab8f7bc51a3847b21f4f025c53a2a
suiranruofeng <jsrdzxw001@163.com>
2025-06-10 17:47:00 +0800

    Update at 2025-06-10 17:47:00

--- plot.py
+++ plot.py
@@ -58,10 +58,10 @@ liu_hours = liu_data['hours']
 liu_level = get_level(liu_data['exp'].sum())
 
 fig = plt.figure(figsize=(10, 6))
-plt.rcParams['font.family'] = 'Noto Sans CJK JP'
+plt.rcParams['font.family'] = 'WenQuanYi Micro Hei'
 ax = fig.add_subplot(111)
 
-ax.plot(zhang_time, zhang_hours, color='blue', ls='-', lw='1', label=f'zhang({zhang_level})')
+ax.plot(zhang_time, zhang_hours, color='blue', ls='-', lw='1', label=f'zhang({zhang_level}门)')
 ax.plot(liu_time, liu_hours, color='red', ls='-', lw='1', label=f'liu({liu_level})')
 ax.set_xlim(left=0, right=liu_time.max() + 1)
 ax.set_ylim(-0.5, 10)
